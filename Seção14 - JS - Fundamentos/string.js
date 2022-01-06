de4aa0812018efdfb8a1633faf183e7c6ae9568c
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-01-06 20:21:16 -0300

    Create string.js

---
+++ Seção14 - JS - Fundamentos/string.js
@@ -0,0 +1,31 @@
+const escola = "C0d3r"
+
+console.log(typeof escola)
+
+console.log(escola.charAt(4)) //pega o elemento no índice 4, ou seja, r
+console.log(escola.charCodeAt(3)) //retorna o valor do elemento no índice 3 de acordo com a tabela ASC, ou seja, d = 100
+console.log(escola.indexOf('0')) //procura o elemento na string e retorna o seu índice, ou seja, 0 índice 1. Caso n exista, ele retorna -1.
+
+console.log(escola.substring(0, 3)) //retorna a string da localização colocada. (0, 3) inicia no índice zero e termina no 3, sem incluir o último índice, sendo ele opcional. Ou seja, C0d
+
+//concatenar: 
+console.log("Escola " .concat(escola).concat("!"))
+
+//substitui um valor por outro
+console.log(escola.replace(3, 'e'))//sustitui o elemento no índice 3 por e 
+console.log(escola.replace(/\d/, 'e')) //sustitui todos os elementos do tipo letra pelo e 
+
+//gerar um Array:
+console.log("Ana, Marcio, Tainara".split(",")) //gera uma string com separação pela vírgula
+
+//expressões:
+
+function up(string){
+    return string.toUpperCase()
+}
+//ou
+
+const up = string => string.toUpperCase()
+
+
+console.log(`Eii... ${up('cuidado')}`)
