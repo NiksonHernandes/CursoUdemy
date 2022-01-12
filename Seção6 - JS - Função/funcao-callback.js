e41fa78398f0ecc1bdeb42843845ceda7fdeccf3
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-01-11 22:49:00 -0300

    Update funcao-callback.js

--- Seção6 - JS - Função/funcao-callback.js
+++ Seção6 - JS - Função/funcao-callback.js
@@ -8,3 +8,27 @@
     }
 
     carros.forEach(imprimir)
+
+//Sem callback
+
+    const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+    let notasBaixas = []
+    for(let indice in notas){
+        if(notas[indice] < 7){
+            notasBaixas.push(notas[indice])
+        }
+    }
+
+    console.log(notasBaixas)
+
+//Com callback
+//O filter()método cria um novo array preenchido com elementos que passam em um teste fornecido por uma função, retorna TRUE ou FALSE
+    
+    function testeValor(nota) {
+        return nota <7 //retorna true ou false
+    }
+
+    notasBaixas = notas.filter(testeValor) //o resultado do notas.filter() vai ser um novo array, ou seja, o notasBaixas.
+
+    console.log(notasBaixas)
