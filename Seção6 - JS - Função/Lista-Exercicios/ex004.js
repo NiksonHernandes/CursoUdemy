da3fd2c1abded23475a08681b098676ce37935c3
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-01-13 21:06:01 -0300

    Curso Udemy

--- Seção6 - JS - Função/Lista-Exercicios/ex004.js
+++ Seção6 - JS - Função/Lista-Exercicios/ex004.js
@@ -1,1 +1,10 @@
 /*Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão destes dois valores.  */
+
+    function divisao(dividendo = 0, divisor = 0){
+        console.log(`o resultado da divisão é: ${dividendo / divisor}`);
+        console.log(`o resto da divisão é: ${dividendo % divisor}`)    
+  
+    }
+
+    divisao(10, 2)
+    divisao(12,4)
