80e2b7bcaa00f405bb9be72bd332909b43642eb0
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-01-15 23:53:08 -0300

    Curso Udemy

--- Seção6 - JS - Função/Lista-Exercicios/ex019.js
+++ Seção6 - JS - Função/Lista-Exercicios/ex019.js
@@ -1,4 +1,4 @@
-​/*O cardápio de uma lanchonete é o seguinte: 
+/*O cardápio de uma lanchonete é o seguinte: 
 Código  Descrição do Produto    Preço 
 100     Cachorro Quente         R$ 3,00 
 200     Hambúrguer Simples      R$ 4,00 
@@ -11,7 +11,28 @@ comando switch. Crie um caso default para produto não existente.*/
 
     function cardapio(codigo, quantidade = 0){
         switch(codigo){
+            //parseFloat(conta.toFixed(2));
             case 100:
-                return `Código 100 - Cachorro Quente - Valor: ${}`
+                return `Código 100 - Cachorro Quente - Valor: ${(quantidade * 3.00).toFixed(2)}`
+            case 200:
+                return `Código 200 - Hambúrguer Simples - Valor: ${(quantidade * 4.00).toFixed(2)}`
+            case 300:
+                return `Código 300 - Cheeseburguer - Valor: ${(quantidade * 5.50).toFixed(2)}`
+            case 400:
+                return `Código 400 - Bauru - Valor: ${(quantidade * 7.50).toFixed(2)}`
+            case 500:
+                return `Código 500 - Refrigerante - Valor: ${(quantidade * 3.50).toFixed(2)}` 
+            case 600:
+                return `Código 700 - Suco - Valor: ${(quantidade * 2.80).toFixed(2)}`  
+            default:
+                return "Código inválido"
         }
     }
+
+    console.log(cardapio(100, 3))
+    console.log(cardapio(200, 3))
+    console.log(cardapio(300, 3))
+    console.log(cardapio(400))
+    console.log(cardapio(500, 3))
+    console.log(cardapio(600, 3))
+    console.log(cardapio(700, 3))
