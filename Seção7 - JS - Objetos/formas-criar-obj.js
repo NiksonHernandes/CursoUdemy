fea30a73a97ac68b85133388b3e7fc3a31eea3f6
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-01-19 21:25:22 -0300

    Create formas-criar-obj.js

---
+++ Seção7 - JS - Objetos/formas-criar-obj.js
@@ -0,0 +1,42 @@
+//usando a notação literal:
+    const obj1 = {}
+    console.log(obj1)
+
+//através do new Object:
+    const obj2 = new Object()
+    console.log(obj2)
+
+//funções construtoras:
+    function obj3(nome, idade , sexo = 'm'){
+        this._nome = nome, //tornou o essa variável(_nome) pública, com o THIS, ela n esta encapsulada dentro do objeto, n esta exclusiva para ele
+        this._sexo = sexo
+
+        this.retornaNome = () => {
+            return idade //idade esta encapsulada, não pode ser mudada apartir do momento q é colocada
+        }
+    }
+
+    const teste1 = new obj3("Nikson", 21, 'Masculino')
+    console.log(teste1.retornaNome())
+  
+
+//Função factory:
+    function obj4(nome, salario, faltas){
+        return{
+            nome: nome,
+            salario,
+            faltas,
+
+            retornaDados() {
+                return `Seu nome: ${nome} \nsalário: ${salario}\nfaltas: ${faltas} `
+            }
+
+        }
+    }
+
+    const teste2 = new obj4("Paulo", 1200, 4)
+    console.log(teste2.retornaDados())
+
+//Object.create 
+    
+
